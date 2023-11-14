63a5d1c7cd2b27aa2bbe50ef807e16ea7f7fd3ac
JN720 <112584623+JN720@users.noreply.github.com>
2023-11-13 22:19:50 -0800

    added UI for buttons to posts

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,5 +1,5 @@
 import { post } from "./Feed";
-import { View, Image, Text, StyleSheet, useWindowDimensions } from 'react-native';
+import { View, Image, Text, StyleSheet, useWindowDimensions, Button } from 'react-native';
 import { randomUUID } from 'expo-crypto';
 
 export default function Post({ item }: { item: post }) {
@@ -17,8 +17,22 @@ export default function Post({ item }: { item: post }) {
         <Text style = {styles.title}>{item.title}</Text>
         <Text style = {styles.content}>{item.text}</Text>
         {item.files.map((uri) => {
-            return <Image style = {styles.image} key = {randomUUID()} width = {dims.width * 0.7} height = {dims.width* 0.7} source = {{uri: uri}}/>
+            return <Image style = {styles.image} key = {randomUUID()} width = {dims.width * 0.7} height = {dims.width * 0.7} source = {{uri: uri}}/>
         })}
+        <View style = {styles.actions}>
+            <View style = {styles.interactView}>
+                <Button color = "pink" title = "Like"/>
+            </View>
+            <View style = {styles.interactView}>
+                <Button color = "red" title = "Dislike"/>
+            </View>
+            <View style = {styles.interactView}>
+                <Button color = "darkblue" title = "Follow"/>
+            </View>
+            <View style = {styles.interactView}>
+                <Button color = "green" title = "Save"/>
+            </View>
+        </View>
     </View>)
 }
 
@@ -59,7 +73,16 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     image: {
-        marginTop: '2%',
+        marginVertical: '2%',
         alignSelf: 'center'
+    },
+    actions: {
+        flexDirection: 'row',
+        marginHorizontal: '7%',
+        marginVertical: '2%',
+        flexShrink: 1
+    },
+    interactView: {
+        marginHorizontal: '2%'
     }
 })
